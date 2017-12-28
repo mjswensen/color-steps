9589473e4391446922cb9072389a350799647ee6
Matt Swensen <swensen.matthew@gmail.com>
2017-12-28 15:29:04 -0700

    Add a few tests.

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const assert = require('assert');
 const color = require('onecolor');
 
+const assertCount = count =>
+  assert(count > 0, `Count must be greater than 0; ${count} was provided`);
+const assertColor = (parsed, original) =>
+  assert(parsed, `The provided color, '${original}', was not parseable`);
+
 module.exports = (color1, color2, count = 6) => {
+  assertCount(count);
   const one = color(color1),
-    two = color(color2),
-    rDelta = (two.red() - one.red()) / (count + 1),
+    two = color(color2);
+  assertColor(one, color1);
+  assertColor(two, color2);
+  const rDelta = (two.red() - one.red()) / (count + 1),
     gDelta = (two.green() - one.green()) / (count + 1),
     bDelta = (two.blue() - one.blue()) / (count + 1),
     aDelta = (two.alpha() - one.alpha()) / (count + 1);
--- index.spec.js
+++ index.spec.js
@@ -4,4 +4,23 @@ describe('color-steps utility', () => {
   it('should match base calucation with white and black', () => {
     expect(colorSteps('black', 'white')).toMatchSnapshot();
   });
+  it('should support alpha', () => {
+    expect(
+      colorSteps('rgba(0, 0%, 0%, 0)', 'rgba(0, 0%, 0%, 1)', 1)
+    ).toMatchSnapshot();
+  });
+  it('should support going from light to dark', () => {
+    expect(colorSteps('white', 'black')).toMatchSnapshot();
+  });
+  it('should throw errors on invalid input', () => {
+    expect(() => {
+      colorSteps('foo', 'bar');
+    }).toThrow(/foo/);
+    expect(() => {
+      colorSteps('red', 'bar');
+    }).toThrow(/bar/);
+    expect(() => {
+      colorSteps('red', 'blue', -2);
+    }).toThrow(/-2/);
+  });
 });
